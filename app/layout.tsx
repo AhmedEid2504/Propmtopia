63171428d358c0ae6ae6fd420b717718d07e8062
Ahmed Eid <ahmedeid2504@gmail.com>
2024-04-14 00:37:54 +0200

    integrates next auth and installs mongodb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "@styles/globals.css";
 import Nav from "@components/Nav";
-import provider from "@components/provider";
+import Provider from "@components/Provider";
 export const metadata: Metadata = {
   title: "Promptopia",
   description: "A platform for users to share their ai prompts",
@@ -11,14 +11,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
---
+++ utils/database.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+let isConnected = false; // Database connection status
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log('Using existing database connection');
+        return;
+    }
+    
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = db.connections[0].readyState;
+        console.log('New database connection established');
+    } catch (error) {
+        console.error('Database connection error:', error);
+    }
+}
+
